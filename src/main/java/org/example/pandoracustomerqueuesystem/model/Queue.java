318b7a9908a38936a52b7658d65bd495a3e231a3
huw02 <hannibal@ussing.com>
2025-04-20 00:01:08 +0200

    initial

--- src/main/java/org/example/pandoracustomerqueuesystem/model/Queue.java
+++ src/main/java/org/example/pandoracustomerqueuesystem/model/Queue.java
@@ -1,4 +1,19 @@
 package org.example.pandoracustomerqueuesystem.model;
 
+import java.util.List;
+
 public class Queue {
+    List<User> queueList;
+
+    public Queue(List<User> queueList) {
+        this.queueList = queueList;
+    }
+
+    public List<User> getQueueList() {
+        return queueList;
+    }
+
+    public void setQueueList(List<User> queueList) {
+        this.queueList = queueList;
+    }
 }
