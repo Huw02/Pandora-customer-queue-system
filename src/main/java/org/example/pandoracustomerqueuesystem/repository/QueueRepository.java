faa70a69b4ad9f361a6044b52d522bb11935252c
huw02 <hannibal@ussing.com>
2025-04-20 00:02:33 +0200

    initial

---
+++ src/main/java/org/example/pandoracustomerqueuesystem/repository/QueueRepository.java
@@ -0,0 +1,4 @@
+package org.example.pandoracustomerqueuesystem.repository;
+
+public class QueueRepository {
+}
---
+++ src/main/java/org/example/pandoracustomerqueuesystem/service/QueueService.java
@@ -0,0 +1,4 @@
+package org.example.pandoracustomerqueuesystem.service;
+
+public class QueueService {
+}
