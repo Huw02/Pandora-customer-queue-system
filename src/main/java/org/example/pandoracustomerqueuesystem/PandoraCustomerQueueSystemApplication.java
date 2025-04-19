35cf020a9cfd08fe7b07367a7b03fab0d9eacec3
huw02 <hannibal@ussing.com>
2025-04-19 22:50:36 +0200

    initial

---
+++ src/main/java/org/example/pandoracustomerqueuesystem/PandoraCustomerQueueSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.pandoracustomerqueuesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandoraCustomerQueueSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PandoraCustomerQueueSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/pandoracustomerqueuesystem/PandoraCustomerQueueSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pandoracustomerqueuesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandoraCustomerQueueSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
