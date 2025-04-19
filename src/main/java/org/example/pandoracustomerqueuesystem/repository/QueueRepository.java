bd24336d31cb60e8be45fa637f4e9e9a272f6202
huw02 <hannibal@ussing.com>
2025-04-20 00:04:50 +0200

    initial

--- src/main/java/org/example/pandoracustomerqueuesystem/repository/QueueRepository.java
+++ src/main/java/org/example/pandoracustomerqueuesystem/repository/QueueRepository.java
@@ -1,4 +1,15 @@
 package org.example.pandoracustomerqueuesystem.repository;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class QueueRepository {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public QueueRepository(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+
+
 }
